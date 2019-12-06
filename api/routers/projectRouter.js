1d4d3eb2f773c3564c4a7e61305f6da5342ff787
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-12-06 12:17:28 -0500

    set up projectRouter file with endpoints and starters code. taking a brk

---
+++ api/routers/projectRouter.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const pm = require('../../data/helpers/projectModel');
+const router = express.Router();
+router.use(express.json());
+
+router.get('/', (req, res) => {
+    //get can take in an id if needed
+});
+
+router.get('/:id', (req, res) => {
+    //get can take in an id if needed
+});
+
+router.get('/:id/actions', (req, res) => {
+    //here
+});
+
+router.delete('/:id', (req, res) => {
+    //here
+});
+
+router.put('/:id', (req, res) => {
+    //here
+});
+
+router.post('/', (req, res) => {
+    //here
+});
+
+// router.post('/:id/actions', (req, res) => {
+//     //here
+// });
